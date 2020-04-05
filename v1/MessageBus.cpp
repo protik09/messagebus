98051eb031af716e386911af2d178b0343c42b57
Protik Banerji <protik09@gmail.com>
2020-04-05 19:03:28 +0200

    Update comments for the MessageBus class members

--- v1/MessageBus.cpp
+++ v1/MessageBus.cpp
@@ -1,43 +1,62 @@
-#include <map>                                 //include map library to store data in mapped fashion
+#include <map> // Include map library to store data in a mapped data type (subscriptions)
 
-#include "MessageBus.h"                       //include user defined function
+#include "MessageBus.h" // Include user defined function
 
-void MessageBus::registerTopic(const std::string& topic)               //function to register topic accepts string data
+// Messagebus member to register topic using string key
+void MessageBus::registerTopic(const std::string &topic)
 {
-	subscriptions[topic] = std::vector<Node*>();                       
+	subscriptions[topic] = std::vector<Node *>();
 }
-
-void MessageBus::registerNode(Node* node) {                           //function creates new nodes
+// Messagebus member to register a new node on the bus
+void MessageBus::registerNode(Node *node)
+{
 	nodes.insert(node);
 }
 
-void MessageBus::registerNodeForTopic(Node* node, std::string topic) {         //function to for linking node and topic
-	std::vector<Node*> elements = subscriptions[topic];
+// Member function to for linking node and topic
+void MessageBus::registerNodeForTopic(Node *node, std::string topic)
+{
+	// Read all node elements into a vector structure
+	std::vector<Node *> elements = subscriptions[topic];
+	// Add new node to the vector structure
 	elements.push_back(node);
+	// Write the modified node structure to the subscription map
 	subscriptions[topic] = elements;
 }
 
-void MessageBus::acceptMessage(TextMessage* message) {                          //function to store messages in queue
+// Member function to store messages in the priority queue
+void MessageBus::acceptMessage(TextMessage *message)
+{
+	// Add message to the bottom of the priority queue
 	queue.push(message);
 }
 
-void MessageBus::printMessages() {                                             //function to print message
-	while (!queue.empty()) {                                                   //checks if queue is not empty 
-		TextMessage tm = *(queue.top());                                       //assigns the pointer data to a variable
-		std::cout << tm.toString() << std::endl;                               //converts and prints in string format
-		queue.pop();                                                            //removes the message after displayiing
+// Messagebus member function to print message
+void MessageBus::printMessages()
+{
+	while (!queue.empty()) // Checks if queue is not empty
+	{											 
+		TextMessage tm = *(queue.top());		 // assigns the pointer data to a variable
+		std::cout << tm.toString() << std::endl; // converts and prints in string format
+		queue.pop();							 // removes the message after displaying
 	}
 }
 
-void MessageBus::handleMessages() {                                             //handleMessages funcction definition 
-	while (queue.size()!=0) {                                                   //loop until queue gets empty
-		TextMessage* message = queue.top();                                     //message pointer holds the info of topmost data on queue
-		
- 		std::vector<Node*> elements = subscriptions[message->getTopic()];            //stores topic information in vector elements
+// Member function to handle 
+void MessageBus::handleMessages()
+{
+	// Process priority queue till empty
+	while (queue.size() != 0)
+	{										
+		TextMessage *message = queue.top(); // Message pointer holds the info of topmost data on queue
 
-		for (Node* node : elements) {                                                //prints all the information in the elements
-			node->acceptMessage(message);
+		std::vector<Node *> elements = subscriptions[message->getTopic()]; //Read all node elements into a vector structure
+
+		// For all elements in a particular topic
+		for (Node *node : elements)
+		{ 	
+			node->acceptMessage(message); // Add the messages to the priority queue
 		}
-		queue.pop();                                                              //removes the data from queue
+		queue.pop(); //removes the data from queue
 	}
 }
